380fd9fe83d5353237d041a407ca2e47a0212646
Felix Icaza <fx.joliett17@gmail.com>
2026-03-20 21:34:20 -0600

    feat: add Background component

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as Picture } from './components/Picture.astro'
 export { default as Image } from './components/Image.astro'
+export { default as Background } from './components/Background.astro'
