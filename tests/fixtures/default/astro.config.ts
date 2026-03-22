6858b77f2f056940f196b5785a496a370c9aa84b
Felix Icaza <fx.joliett17@gmail.com>
2026-03-22 16:46:54 -0600

    chore: update tests with remote images

--- tests/fixtures/default/astro.config.ts
+++ tests/fixtures/default/astro.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  compressHTML: false
+  compressHTML: false,
+  image: {
+    domains: ['images.pexels.com']
+  }
 })
